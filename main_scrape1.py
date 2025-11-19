83eaf2dedff6a94ff9b7db9c55d9e981fccb7c85
kzysg <zilcd.dje.ky3@gmail.com>
2025-11-19 11:09:18 +0900

    Update main_scrape1.py

--- main_scrape1.py
+++ main_scrape1.py
@@ -50,16 +50,8 @@ if len(driver.window_handles) > 1:
     driver.switch_to.window(driver.window_handles[-1])
     time.sleep(3)
 
-# チェックボックス操作（世田谷区・大田区・板橋区）
-#for value in ["12", "11", "19"]:
-#    try:
-#        checkbox = driver.find_element(By.CSS_SELECTOR, f'input[value="{value}"][type="checkbox"]')
-#        checkbox.click()
-#        time.sleep(0.5)
-#    except:
-#        pass
-# チェックボックス操作（板橋区）
-for value in ["19"]:
+#チェックボックス操作（世田谷区・大田区・板橋区）
+for value in ["12", "11", "19"]:
     try:
         checkbox = driver.find_element(By.CSS_SELECTOR, f'input[value="{value}"][type="checkbox"]')
         checkbox.click()
